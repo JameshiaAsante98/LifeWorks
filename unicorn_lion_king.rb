989ea20bbd592cf5fd8050f837f2ca57c2e80cb3
JameshiaAsante98 <137513219+JameshiaAsante98@users.noreply.github.com>
2023-06-25 09:43:16 +0700

    Fix UI layout issues

---
+++ unicorn_lion_king.rb
@@ -0,0 +1,207 @@
+1. class CounselingService
+2. 
+3.   def help_clients(challenges)
+4.     challenges.each do |challenge|
+5.       puts "We are here to help you overcome #{challenge}."
+6.     end
+7.   end
+8. 
+9.   def find_purpose(clients)
+10.     clients.each do |client|
+11.       puts "We will help #{client} find purpose in life."
+12.     end
+13.   end
+14.
+15.   def help_fulfill(goals)
+16.     goals.each do |goal|
+17.       puts "We are here to help you fulfill #{goal}."
+18.     end
+19.   end
+20.
+21.   def provide_support(problems)
+22.     problems.each do |problem|
+23.       puts "We will provide you with support to resolve #{problem}."
+24.     end
+25.   end 
+26.
+27.   def give_encouragement(tasks)
+28.     tasks.each do |task|
+29.       puts "We are here to encourage you to complete #{task}."
+30.     end
+31.   end
+32.
+33.   def provide_empathy(clients)
+34.     clients.each do |client|
+35.       puts "We will provide empathy to support #{client} in their journey."
+36.     end
+37.   end
+38.
+39.   def suggest_solutions(issues)
+40.     issues.each do |issue|
+41.       puts "We will help you explore solutions to your #{issue}."
+42.     end
+43.   end
+44.
+45.   def encourage_change(habits)
+46.     habits.each do |habit|
+47.       puts "We will encourage you to make positive changes to your #{habit}."
+48.     end
+49.   end
+50.
+51.   def create_strategies(goals)
+52.     goals.each do |goal|
+53.       puts "We will help you create strategies to reach your #{goal}."
+54.     end
+55.   end
+56.
+57.   def give_guidance(dreams)
+58.     dreams.each do |dream|
+59.       puts "We are here to guide you in achieving your #{dream}."
+60.     end
+61.   end
+62.
+63.   def provide_structure(tasks)
+64.     tasks.each do |task|
+65.       puts "We are here to provide structure for you to complete #{task}"
+66.     end
+67.   end
+68.
+69.   def give_insight(issues)
+70.     issues.each do |issue|
+71.       puts "We will provide insight to help you address your #{issue}."
+72.     end
+73.   end
+74.
+75.   def mentor(steps)
+76.     steps.each do |step|
+77.       puts "We will mentor you through the #{step} of achieving your goals."
+78.     end
+79.   end
+80.
+81.   def inspire_hope(clients)
+82.     clients.each do |client|
+83.       puts "We will inspire hope in #{client} to pursue their dreams."
+84.     end
+85.   end
+86.
+87.   def provide_resources(opportunities)
+88.     opportunities.each do |opportunity|
+89.       puts "We will provide you with the resources to take advantage of #{opportunity}."
+90.     end
+91.   end
+92.
+93.   def foster_growth(achievements)
+94.     achievements.each do |achievement|
+95.       puts "We will foster growth so that you can realize your #{achievement}."
+96.     end
+97.   end
+98.
+99.   def encourage_self_expression(clients)
+100.    clients.each do |client|
+101.      puts "We will encourage #{client} to express themselves and be their authentic selves."
+102.    end
+103.  end
+104.
+105.  def provide_empowerment(goals)
+106.    goals.each do |goal|
+107.      puts "We will provide you with the empowerment to achieve your #{goal}."
+108.    end
+109.  end
+110.
+111.  def foster_self_confidence(clients)
+112.    clients.each do |client|
+113.      puts "We foster self confidence within #{client} to help them reach their potential."
+114.    end
+115.  end
+116.
+117.  def give_clarity(tasks)
+118.    tasks.each do |task|
+119.      puts "We will help provide you with clarity to successfully complete #{task}."
+120.    end
+121.  end
+122.
+123.  def provide_meaning(aspirations)
+124.    aspirations.each do |aspiration|
+125.      puts "We will help provide meaning to #{aspiration} and move closer to achieving it."
+126.    end
+127.  end
+128.
+129.  def offer_clarity(ideas)
+130.    ideas.each do |idea|
+131.      puts "We will offer clarity to help you understand your #{idea}."
+132.    end
+133.  end
+134.
+135.  def give_guidance_on_decisions(clients)
+136.    clients.each do |client|
+137.      puts "We will provide guidance to #{client} so they can make sound decisions."
+138.    end
+139.  end
+140.
+141.  def provide_encouragement(clients)
+142.    clients.each do |client|
+143.      puts "We will provide encouragement to #{client} to stay the course."
+144.    end
+145.  end
+146.
+147.  def find_happiness(clients)
+148.    clients.each do |client|
+149.      puts "We will help #{client} discover true happiness."
+150.    end
+151.  end
+152.
+153.  def create_awareness(goals)
+154.    goals.each do |goal|
+155.      puts "We will help you create awareness of how to achieve your #{goal}."
+156.    end
+157.  end
+158.
+159.  def provide_inspiration(aspirations)
+160.    aspirations.each do |aspiration|
+161.      puts "We are here to provide inspiration to help you pursue your #{aspiration}."
+162.    end
+163.  end
+164.
+165.  def create_transformative_change(habits)
+166.    habits.each do |habit|
+167.      puts "We are here to create transformative change in your #{habit}."
+168.    end
+169.  end
+170.
+171.  def give_positive_reinforcement(clients)
+172.    clients.each do |client|
+173.      puts "We will provide positive reinforcement to empower #{client}."
+174.    end
+175.  end
+176.
+177.  def inspire_motivation(tasks)
+178.    tasks.each do |task|
+179.      puts "We are here to inspire motivation to help you complete #{task}."
+180.    end
+181.  end
+182.
+183.  def challenge_limiting_beliefs(clients)
+184.    clients.each do |client|
+185.      puts "We will challenge #{client}'s limiting beliefs to help them reach their goals."
+186.    end
+187.  end
+188.
+189.  def help_discover_strengths(clients)
+190.    clients.each do |client|
+191.      puts "We are here to help #{client} discover their strengths and use them to create a more meaningful life."
+192.    end
+193.  end
+194.
+195.  def provide_challenges(clients)
+196.    clients.each do |client|
+197.      puts "We will provide #{client} with challenges that will help them succeed."
+198.    end
+199.  end
+200.
+201.  def provide_stability(clients)
+202.    clients.each do |client|
+203.      puts "We will provide #{client} with the stability they need to overcome life's challenges and find purpose and fulfillment"
+204.    end
+205.  end
+206. 
+207.end
